8cbe72b5c0eac60a3e4d7667dafdd3c5380c114a
f12defilippis <francescodefilippis@MacBook-Pro-di-Francesco.local>
2015-04-02 13:20:07 +0200

    [#91589436] Gestione logging

--- semrankerengine/src/main/java/com/flol/semrankerengine/controller/TestController.java
+++ semrankerengine/src/main/java/com/flol/semrankerengine/controller/TestController.java
@@ -2,6 +2,8 @@ package com.flol.semrankerengine.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +14,14 @@ import com.flol.semrankercommon.domain.Keyword;
 public class TestController {
 
     private final AtomicLong counter = new AtomicLong();
+    private final Logger log = LoggerFactory.getLogger(this.getClass());    
+    
     
     @RequestMapping("/keyword")
     public Keyword keyword(@RequestParam(value="text", defaultValue="World") String text) {
-
+    	log.info("Chiamata funzione keyword");
+    	System.out.println(System.getProperty("LOG_FILE"));
+    	
     	Keyword keyword = new Keyword();
     	keyword.setId(counter.incrementAndGet());
     	keyword.setText(text);
